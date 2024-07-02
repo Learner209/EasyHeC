f8e08f2eb594d5c2a32a4d974a447b5ade2f6375
Learner209 <yufeiyang553@gmail.com>
2024-07-02 17:48:54 +0800

    Update default config: delete key [easyhec_repo_path].

--- easyhec/config/defaults_franka.py
+++ easyhec/config/defaults_franka.py
@@ -7,7 +7,6 @@ _C.dbg = True
 _C.evaltime = False
 _C.deterministic = False
 _C.backup_src = True
-_C.easyhec_repo_path = "/home/liilu/Desktop/robotflow/easy_calib/third_party/easyhec"
 _C.use_xarm = False
 
 _C.sim_mask_data = CN()
